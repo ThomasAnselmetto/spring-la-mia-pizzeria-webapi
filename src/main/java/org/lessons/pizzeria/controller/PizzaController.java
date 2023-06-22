42f908363e7a002ca47d8233cf6b927109f9851d
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-06-22 15:38:25 +0200

    init commit

---
+++ src/main/java/org/lessons/pizzeria/controller/PizzaController.java
@@ -0,0 +1,21 @@
+package org.lessons.pizzeria.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pizza")
+public class PizzaController {
+
+
+//    PathVariable lo dichiariamo solo quando nel path va qualcosa di breve o di chiaro tipo un id
+
+//    @GetMapping()
+//    public String index(Model model){
+//        model.addAttribute("menu",)
+//    }
+
+//@GetMapping("/{id}")
+////ricorda il trycatch con outofbound
+//@PathVariable
+}
---
+++ src/main/java/org/lessons/pizzeria/model/Pizza.java
@@ -0,0 +1,53 @@
+package org.lessons.pizzeria.model;
+
+import java.math.BigDecimal;
+
+
+public class Pizza {
+    private String nome;
+    private String descrizione;
+    private String fotoUrl;
+    private BigDecimal prezzo;
+
+    public Pizza(String nome, String descrizione, String fotoUrl, BigDecimal prezzo) {
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.fotoUrl = fotoUrl;
+        this.prezzo = prezzo;
+    }
+
+    //quando si lavora con i DB definire SEMPRE i getter e setter
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getFotoUrl() {
+        return fotoUrl;
+    }
+
+    public void setFotoUrl(String fotoUrl) {
+        this.fotoUrl = fotoUrl;
+    }
+
+    public BigDecimal getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(BigDecimal prezzo) {
+        this.prezzo = prezzo;
+    }
+}
+
